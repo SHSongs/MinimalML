dc8ff209a407c2239bb15e08c6ebd8debe79aa23
Song <217.Songs.SH@gmail.com>
2021-01-21 22:37:35 +0900

    README.md update

--- AI/Linear regression/Linear regression.py
+++ AI/Linear regression/Linear regression.py
@@ -1,44 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x_train = np.array([1., 2., 3., 4., 5., 6.])
-y_train = np.array([9., 16., 23., 30., 37., 44.])
-
+y_train = np.array([5., 14., 20., 34., 35., 40.])
 
 W = 0.0
 b = 0.0
 
 n_data = len(x_train)
 
-epochs = 5000
+epochs = 1000
 learning_rate = 0.01
 
 for i in range(epochs):
     h = 0.001
 
     hypothesis = x_train * W + b
-
-    hypothesis2_w_up = x_train * (W + h) + b
-    hypothesis2_b_up = x_train * W + (b + h)
-
-    hypothesis2_w_down = x_train * (W - h) + b
-    hypothesis2_b_down = x_train * W + (b - h)
-
     cost = np.sum((hypothesis - y_train) ** 2) / n_data
 
-    cost2_w_up = np.sum((hypothesis2_w_up - y_train) ** 2) / n_data
-    cost2_b_up = np.sum((hypothesis2_b_up - y_train) ** 2) / n_data
+    hypothesis_w_up = x_train * (W + h) + b
+    hypothesis_b_up = x_train * W + (b + h)
 
-    cost2_w_down = np.sum((hypothesis2_w_down - y_train) ** 2) / n_data
-    cost2_b_down = np.sum((hypothesis2_b_down - y_train) ** 2) / n_data
+    hypothesis_w_down = x_train * (W - h) + b
+    hypothesis_b_down = x_train * W + (b - h)
 
-    numerical_grad_w = (cost2_w_up - cost2_w_down)*2 / h
-    numerical_grad_b = (cost2_b_up - cost2_b_down)*2 / h
+    cost_w_up = np.sum((hypothesis_w_up - y_train) ** 2) / n_data
+    cost_b_up = np.sum((hypothesis_b_up - y_train) ** 2) / n_data
 
-    gradient_w = np.sum((W * x_train - y_train + b) * 2 * x_train) / n_data
-    gradient_b = np.sum((W * x_train - y_train + b) * 2) / n_data
+    cost_w_down = np.sum((hypothesis_w_down - y_train) ** 2) / n_data
+    cost_b_down = np.sum((hypothesis_b_down - y_train) ** 2) / n_data
+
+    numerical_grad_w = (cost_w_up - cost_w_down) * 2 / h
+    numerical_grad_b = (cost_b_up - cost_b_down) * 2 / h
 
     W -= learning_rate * numerical_grad_w
     b -= learning_rate * numerical_grad_b
+
+    # 편미분
+    gradient_w = np.sum((W * x_train - y_train + b) * 2 * x_train) / n_data
+    gradient_b = np.sum((W * x_train - y_train + b) * 2) / n_data
 
     if i % 100 == 0:
         print('Epoch ({:10d}/{:10d}) cost: {:10f}, W: {:10f}, b:{:10f}'.format(i, epochs, cost, W, b))
@@ -47,3 +47,11 @@ print('W: {:10f}'.format(W))
 print('b: {:10f}'.format(b))
 print('result : ')
 print(x_train * W + b)
+
+x_predict = np.array(list(range(1, 7)))
+y_predict = x_predict * W + b
+
+plt.plot(x_train, y_train, 'or', label='origin data')
+plt.plot(x_predict, y_predict, 'b', label='predict')
+plt.legend(['origin', 'predict'])
+plt.show()
--- AI/Linear regression/README.md
+++ AI/Linear regression/README.md
@@ -2,6 +2,55 @@
 
 ## y = Wx+b
 
+![Linear](imgs/Linearregression.png)
+![IncorrectlyLinear](imgs/IncorrectlylabeledData.png)
+## 수치미분으로 구하기
+
+예측하는 parameters  
+W, b  
+각각 미분해줘야 한다   
+-------------------------------------
+
+
+h 만큼 증가된 hypothesis
+```python
+hypothesis_w_up = x_train * (W + h) + b
+hypothesis_b_up = x_train * W + (b + h)
+```
+
+h 만큼 감소된 hypothesis
+```python
+hypothesis_w_down = x_train * (W - h) + b
+hypothesis_b_down = x_train * W + (b - h)
+```
+
+up 된 cost 구하기
+```python
+cost_w_up = np.sum((hypothesis_w_up - y_train) ** 2) / n_data
+cost_b_up = np.sum((hypothesis_b_up - y_train) ** 2) / n_data
+```
+
+down 된 cost 구하기
+```python
+cost_w_down = np.sum((hypothesis_w_down - y_train) ** 2) / n_data
+cost_b_down = np.sum((hypothesis_b_down - y_train) ** 2) / n_data
+```
+
+up과 down 된 cost 로 기울기 구하기
+```python
+numerical_grad_w = (cost_w_up - cost_w_down) * 2 / h
+numerical_grad_b = (cost_b_up - cost_b_down) * 2 / h
+```
+
+learning_rate 만큼 학습하기
+```python
+W -= learning_rate * numerical_grad_w
+b -= learning_rate * numerical_grad_b
+```
+
 ## 편미분으로 구하기
 
-## 수치미분으로 구하기
+```python
+gradient_w = np.sum((W * x_train - y_train + b) * 2 * x_train) / n_data
+gradient_b = np.sum((W * x_train - y_train + b) * 2) / n_data
+```
