406b3d0fab2379456b03da7f52d7e0a9df230c61
Song <217.Songs.SH@gmail.com>
2021-02-10 12:47:55 +0900

    test data

---
+++ AI/unet/display_results.py
@@ -0,0 +1,39 @@
+##
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+##
+result_dir = './results/numpy'
+
+lst_data = os.listdir(result_dir)
+
+lst_label = [f for f in lst_data if f.startswith('label')]
+lst_input = [f for f in lst_data if f.startswith('input')]
+lst_output = [f for f in lst_data if f.startswith('output')]
+
+
+lst_label.sort()
+lst_input.sort()
+lst_output.sort()
+
+##
+id = 0
+
+label = np.load(os.path.join(result_dir, lst_label[id]))
+input = np.load(os.path.join(result_dir, lst_input[id]))
+output = np.load(os.path.join(result_dir, lst_output[id]))
+
+##
+plt.subplot(131)
+plt.imshow(label)
+plt.title('label')
+
+plt.subplot(132)
+plt.imshow(input)
+plt.title('input')
+
+plt.subplot(133)
+plt.imshow(output)
+plt.title('output')
+
+plt.show()
