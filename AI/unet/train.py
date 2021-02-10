777505dc06e6441a1b52ecb7b3906305c3ddc138
Song <217.Songs.SH@gmail.com>
2021-02-10 11:34:03 +0900

    train

--- AI/unet/train.py
+++ AI/unet/train.py
@@ -4,16 +4,17 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from model import UNet
+import torch.nn as nn
 
 from torch.utils.data import DataLoader
-# from torch.utils.tensorboard import SummaryWriter
 
 from torchvision import transforms, datasets
 
+from torch.utils.tensorboard import SummaryWriter
 ##
 lr = 1e-4
-batch_size = 4
-num_epoch = 100
+batch_size = 2
+num_epoch = 5
 
 data_dir = './datasets'
 ckpt_dir = './checkpoint'
@@ -114,7 +115,139 @@ class RandomFlip(object):
 
 ##
 transform = transforms.Compose([Normalization(), RandomFlip(), ToTensor()])
-dataset_train = Dataset(data_dir='./datasets/train', transform=transform)
+
+dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
+
+dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
+loader_val = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
+
+##
+net = UNet().to(device)
+
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+##
+
+num_data_train = len(dataset_train)
+num_data_val = len(dataset_val)
+
+num_batch_train = np.ceil(num_data_train / batch_size)
+num_batch_val = np.ceil(num_data_val / batch_size)
+
+##
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * (x > 0.5)
+
+##
+writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+
+## network save
+
+def save(ckpt_dir, net, optim, epoch):
+
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()}, "./%s/model_epoch%d.pth" % (ckpt_dir, epoch))
+
+## network load
+
+def load(ckpt_dir, net, optim):
+    if not os.path.exists(ckpt_dir):
+        epoch = 0
+        return net, optim, epoch
+
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+    dict_model = torch.load('%s/%s' % (ckpt_dir, ckpt_lst[-1]))
+
+    net.load_state_dict(dict_model['net'])
+    optim.load_state_dict(dict_model['optim'])
+    epoch = int(ckpt_lst[-1].split('epoch')[1].split('.pth')[0])
+
+    return net, optim, epoch
+
+
+
+##
+
+st_epoch = 0
+net, optim, epoch = load(ckpt_dir, net, optim)
+
+for epoch in range(st_epoch + 1, num_epoch +1):
+    net.train()
+    loss_arr = []
+
+    for batch, data in enumerate(loader_train,1):
+
+        label = data['label'].to(device)
+        input = data['input'].to(device)
+
+        output = net(input)
+
+        # backword ass
+        optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        loss.backward()
+
+        optim.step()
+
+        loss_arr += [loss.item()]
+
+        print("TRAIN:  EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+              (epoch, num_epoch, batch_size, num_batch_train, np.mean(loss_arr)))
+
+        # Tensorboard save
+        label = fn_tonumpy(label)
+        input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+        output = fn_tonumpy(fn_class(output))
+
+        writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+
+    writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    with torch.no_grad():
+        net.eval()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_val, 1):
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = net(input)
+
+            loss = fn_loss(output, label)
+
+            loss_arr += [loss.item()]
+
+            print("VALID:  EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                  (epoch, num_epoch, batch_size, num_batch_train, np.mean(loss_arr)))
+
+            # Tensorboard save
+            label = fn_tonumpy(label)
+            input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+            output = fn_tonumpy(fn_class(output))
+
+            writer_val.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+    writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    if epoch % 5 == 0:
+        save(ckpt_dir, net, optim, epoch)
+
+writer_train.close()
+writer_val.close()
 
 ##
 data = dataset_train.__getitem__(0)
@@ -132,4 +265,5 @@ plt.imshow(input.squeeze())
 plt.title('input')
 
 plt.show()
+
 
