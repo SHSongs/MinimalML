ca28d3e3a185f2e85f4dc78160d5b753a04b46a7
Song <217.Songs.SH@gmail.com>
2021-04-06 13:34:42 +0900

    Create loss.py

---
+++ AI/YOLO/loss.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+
+def IOU(boxes_preds, boxes_labels):
+    box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+    box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+    box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+    box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+
+    box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+    box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+    box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+    box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+
+class YoloLoss(nn.Module):
+
+    def __init__(self, S=7, B=2, C=20):
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+
+        self.S = S
+        self.B = B
+        self.C = C
+
+        self.lambda_coord = 5
+        self.lambda_noobj = 0.5
+
+    def forward(self, predictions, target):
+        # predictions size BATCH_SIZE, S*S*(C+B*5)
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
+        iou_b1 = IOU(predictions[..., 21:25], target[..., 21:25]) # 1,7,7,1
+        iou_b2 = IOU(predictions[..., 26:30], target[..., 21:25])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0) # 2,1,7,7,1
+
+        iou_maxes, bestbox = torch.max(ious, dim=0) # 1,7,7,1
